654315cb6896b7368a06786dd4e9a4c70502b7dc
Hector Mejia <mejia7hector@gmail.com>
2022-07-04 00:02:28 -0500

    styling

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -6,22 +6,24 @@ import Layout from '../../components/layout';
 export default function FirstPost() {
     return (
         <>
-            <Head>
-                <title>First Post</title>
-            </Head>
-            {/*<Script*/}
-            {/*    src="https://connect.facebook.net/en_US/sdk.js"*/}
-            {/*    strategy="Lazyonload"*/}
-            {/*    onLoad={() =>*/}
-            {/*        console.log(`script loaded correctly, window.FB has been populated`)*/}
-            {/*    }*/}
-            {/*/>*/}
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                {/*<Script*/}
+                {/*    src="https://connect.facebook.net/en_US/sdk.js"*/}
+                {/*    strategy="Lazyonload"*/}
+                {/*    onLoad={() =>*/}
+                {/*        console.log(`script loaded correctly, window.FB has been populated`)*/}
+                {/*    }*/}
+                {/*/>*/}
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </Layout>
         </>
     );
 }
