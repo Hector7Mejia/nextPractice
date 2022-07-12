9e8211c68f25b48a2672c065c01b4a026a902b88
Hector Mejia <mejia7hector@gmail.com>
2022-07-11 22:26:30 -0500

    fixed syntax

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,12 +1,16 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-    // Add the "await" keyword like this:
     const postData = await getPostData(params.id);
-    // ...
+    return {
+        props: {
+            postData,
+        },
+    };
 }
 
 export async function getStaticPaths() {
@@ -20,16 +24,12 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
         <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            {/*{postData.title}*/}
-            {/*<br />*/}
+            {postData.title}
+            <br />
             {postData.id}
             <br />
-            <Date dateString={postData.date} />
-            {/*{postData.date}*/}
-            {/*<br />*/}
+            {postData.date}
+            <br />
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     );
